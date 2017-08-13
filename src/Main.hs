09680eb7fa79acf58586457a7cafff30e2b3ecf7
Alexey Raga <alexey.raga@gmail.com>
2017-08-14 08:32:11 +1000

    Simpler reproducing

--- src/Main.hs
+++ src/Main.hs
@@ -2,64 +2,21 @@
 module Main where
 
 import           Control.Monad
-import           Data.Avro
-import           Data.Avro.Schema
-import qualified Data.Avro.Types    as AT
-import           Data.List.NonEmpty
 import           Data.Monoid        ((<>))
-import           Data.Text
-
-data Message = Message
-  { messageTo   :: Maybe Text
-  , messageText :: Text
-  } deriving (Show, Eq)
-
-messageSchema :: Schema
-messageSchema =
-  Record "Message" Nothing [] Nothing Nothing
-    [ fld "to"        (mkUnion $ Null :| [String])  Nothing
-    , fld "text"      String                        Nothing
-    ]
-    where
-    fld nm ty def = Field nm [] Nothing Nothing ty def
-
-instance ToAvro Message where
-  schema = pure messageSchema
-  toAvro msg = record messageSchema
-            [ "to"         .= messageTo msg
-            , "text"       .= messageText msg
-            ]
-
-instance FromAvro Message where
-  fromAvro (AT.Record _ r) =
-    Message <$> r .: "to"
-            <*> r .: "text"
-  fromAvro r = badValue r "Message"
-
+import           Data.Text          (Text)
+import           Data.Text.Encoding (decodeUtf8, encodeUtf8)
 
 main :: IO ()
 main =
-  let msg = Message (Just "foo") "I love you"
-  in mapM_ (assert msg) [1..20]
+  mapM_ assertText (replicate 20 "I love you")
 
-
-assert :: Message -> a -> IO ()
-assert msg _ = do
-  let res = roundtrip msg
-  case res of
-    Left err -> do
+assertText :: Text -> IO ()
+assertText msg = do
+  let res = decodeUtf8 $ encodeUtf8 msg
+  if res /= msg
+    then do
       putStrLn $ "Expected: " <> show msg
-      putStrLn $ "Actual:   " <> show err
-      error err
-    Right msg' | msg /= msg' -> do
-      putStrLn $ "Expected: " <> show msg
-      putStrLn $ "Actual:   " <> show msg'
+      putStrLn $ "Actual:   " <> show res
       putStrLn "-------------------------------------------------------------"
-    _ -> return ()
+    else return ()
 
-roundtrip :: Message -> Either String Message
-roundtrip msg = resultToEither $ decode (schemaOf msg) (encode msg)
-
-resultToEither :: Result a -> Either String a
-resultToEither (Success a) = Right a
-resultToEither (Error e)   = Left e
